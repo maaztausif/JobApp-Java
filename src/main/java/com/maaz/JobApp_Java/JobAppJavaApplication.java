f303150cda65c09f5884f0d8e346cb033bc549a9
maaztausif <maaztausif48@gmail.com>
2025-09-03 20:32:32 +0200

    Project Created

---
+++ src/main/java/com/maaz/JobApp_Java/JobAppJavaApplication.java
@@ -0,0 +1,13 @@
+package com.maaz.JobApp_Java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobAppJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobAppJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maaz/JobApp_Java/JobAppJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maaz.JobApp_Java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobAppJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
